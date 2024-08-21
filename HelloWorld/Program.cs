be22c0a8ad354f644db7d5d10bd32a1504f7c35a
Darenk625 <deniss.kuzmins07@gmail.com>
2024-08-21 15:00:28 +0200

    Tränat på nytt projekt, konsol-komandom

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Hej världen!");
+Console.WriteLine("Ты мразь!");
+Console.Write("Пидор");
+string namn = Console.ReadLine();
+Console.WriteLine("Сам ты" + namn + "блять")
+
+Console.BackgroundColor = ConsoleColor.Yellow
+Console.ForegroundColor = ConsoleColor.DarkBlue
+Console.WriteLine("Såg du på OS i sommar?")
+Console.WriteLine("VIlken sport gillar du mest?");
