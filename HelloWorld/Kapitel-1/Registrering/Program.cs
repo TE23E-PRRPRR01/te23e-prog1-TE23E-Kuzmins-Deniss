ba33738fea6f2a4226287286127402410b66a070
Deniss Kuzmins <deniss.kuzmins@elev.ga.ntig.se>
2024-08-26 14:32:35 +0200

    Tränat på cw, cr och svenska tecken.

---
+++ HelloWorld/Kapitel-1/Registrering/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Program för att registrera förnamn och efternamn...");
+Console.Write("Ange förnamn:");
+string förnamn = Console.ReadLine();
+Console.Write("Ange efternamn:");
+string efternamn = Console.ReadLine();
+Console.WriteLine("Hej! " + förnamn + " " + efternamn);
+Console.WriteLine($"Hej! {Förnamn} {efternamn}");
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,5 +8,5 @@ Console.WriteLine("Сам ты" + namn + "блять")
 
 Console.BackgroundColor = ConsoleColor.Yellow
 Console.ForegroundColor = ConsoleColor.DarkBlue
-Console.WriteLine("Såg du på OS i sommar?")
+Console.WriteLine("Såg du på OS i sommar?")                                     
 Console.WriteLine("VIlken sport gillar du mest?");
