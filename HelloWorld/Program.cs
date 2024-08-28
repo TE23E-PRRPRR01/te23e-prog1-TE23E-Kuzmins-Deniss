7e1fae40ac5408a152e255b9482ba230f763a1f2
Deniss Kuzmins <deniss.kuzmins@elev.ga.ntig.se>
2024-08-28 14:42:46 +0200

    Lycktext, Rolig ryska

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,9 +4,9 @@ Console.WriteLine("Hej världen!");
 Console.WriteLine("Ты мразь!");
 Console.Write("Пидор");
 string namn = Console.ReadLine();
-Console.WriteLine("Сам ты" + namn + "блять")
+Console.WriteLine("Сам ты" + namn + "блять");
 
 Console.BackgroundColor = ConsoleColor.Yellow
 Console.ForegroundColor = ConsoleColor.DarkBlue
-Console.WriteLine("Såg du på OS i sommar?")                                     
+Console.WriteLine("Såg du på OS i sommar?");                       
 Console.WriteLine("VIlken sport gillar du mest?");
