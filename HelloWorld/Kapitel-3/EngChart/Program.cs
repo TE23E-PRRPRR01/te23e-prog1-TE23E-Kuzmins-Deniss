5797525aaf1a5f8d2bdda63cfa8b15dcc79e5ee1
Deniss Kuzmins <deniss.kuzmins@elev.ga.ntig.se>
2024-09-02 14:51:29 +0200

    Tränat på if-satsen

---
+++ HelloWorld/Kapitel-3/EngChart/Program.cs
@@ -0,0 +1,50 @@
+﻿// Program som simulerar kända Engineering Flowchart
+using System.Xml.Schema;
+
+Console.Clear();
+Console.WriteLine("Välkommen till ingenjörens flödesschema");
+
+//Visa en ASCii art
+Console.WriteLine($"""
+
+                                 _..-------++._
+                             _.-'/ |      _||  \"--._
+                       __.--'`._/_\j_____/_||___\    `----.
+                  _.--'_____    |          \     _____    /
+                _j    /,---.\   |        =o |   /,---.\   |_
+               [__]==// .-. \\==`===========/==// .-. \\=[__]
+                 `-._|\ `-' /|___\_________/___|\ `-' /|_.'     hjw
+                       `---'                     `---'
+""");
+
+Console.Write("Does it move? (Yes/No)");
+string answer = Console.ReadLine();
+
+//Kolla om svara ja eller nej
+if (answer == "Yes")
+{ // Yes
+    Console.Write("Should it? (Yes/No)");
+    answer = Console.ReadLine();
+    if (answer == "Yes")
+    {
+        Console.WriteLine("No problem!");
+    }
+    else
+    {
+        Console.WriteLine("Plz use Duct Tape!");
+    }
+}
+else
+{ // No
+        Console.Write("Should it? (Yes/No)");
+ answer = Console.ReadLine();
+ if (answer == "Yes")
+ {
+    Console.WriteLine("Plz use WD-40!");
+ }
+ else
+ {
+    Console.WriteLine("No problem!");
+ }
+ 
+}
