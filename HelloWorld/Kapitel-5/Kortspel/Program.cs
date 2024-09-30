a5a9af1ab39d3635c2c9512f7ec0b47d51f6051f
Deniss Kuzmins <deniss.kuzmins@elev.ga.ntig.se>
2024-09-30 14:53:00 +0200

    Vi har tränat på list, sloomp, unicode och emoji

---
+++ HelloWorld/Kapitel-5/Kortspel/Program.cs
@@ -0,0 +1,30 @@
+﻿//Ett anna exermpel. Lista på 3 årtal   
+//List<int> årtal = [1957, 1969, 1989];
+//Hur man skapar och anväbder listor
+using System.ComponentModel;
+
+Console.Clear();
+Console.WriteLine("Slumpa kort ur ett kortlek!");
+
+//Skapa en lista kort
+List<string> kortlek = ["Ess","Tvåa","Trea","Fyra","Femma","Sexa","Sjua","Åtta","Nia","Tia","Knekt","Dam","Kung"];
+
+internal antal = 5;
+while (true)
+{
+//Slumpa index 0-12
+int index = Random.Shared.Next(0, kortlek.Count);
+//Console.WriteLine($"Det slumpade index = {index}");
+
+//Plocka ut det sjunde kortet, index = 6
+string kort = kortlek[index];
+
+//Ta bort kortet ur kortleket
+kortlek.RemoveAt(index);
+
+//Skriv ut 7:e kortet
+Console.WriteLine($"Det slumpade jortet är {kort}");
+
+//Räkna ned
+antal--;
+}
