2a5e3cf07b28d68e40924c7e5c2206c7bac9bd28
Deniss Kuzmins <deniss.kuzmins@elev.ga.ntig.se>
2025-01-08 15:03:25 +0100

    Vi har tränat på metoder

---
+++ HelloWorld/Kapitel-6/Metoderintro/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.Clear();
+Console.WriteLine("Hej metoder!");
+
+//Anropa/använd metosen
+SägHEj();
+SägHEj();
+SägHEj();
+SägHEj();
+SägHEj();
+
+//Anropa med metoden meny
+VIsaMeny();
+
+
+//Anropa metoden Registrera namn
+RegistreraNman();
+
+
+
+/* *********************************************************************************************************************************
+***************************  METODER ***********************************************************************
+**************************************************************************************************************************** */
+
+/// <summary>
+/// Metod som skriver ut en hälsning
+/// </summary>
+
+static void SägHEj()
+{
+    Console.WriteLine("Hej på dig!");
+}
+
+/// <summary>
+/// Metod för att visa en meny
+/// </summary>
+
+static void VIsaMeny()
+{
+    Console.WriteLine("""
+    1.Lägg till uppgift
+    2.Ta bort uppgift
+    3.Skriv ut alla uppgifter
+    4.Avsluta
+    """);
+}
+
+/// <summary>
+/// Metod för att ange namn
+/// </summary>
+static void RegistreraNman()
+{
+Console.Write("Ange namn: ");
+string namn = Console.ReadLine();
+Console.WriteLine(namn);
+}
